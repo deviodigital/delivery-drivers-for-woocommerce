b640ac0359e5d6c22b2d05644b4031c0d47a8351
Robert DeVore <deviodigital@gmail.com>
2018-08-31 09:01:58 -0400

    :book: DOC: Updated plugin details

--- delivery-drivers-for-woocommerce.php
+++ delivery-drivers-for-woocommerce.php
@@ -9,11 +9,11 @@
  *
  * @wordpress-plugin
  * Plugin Name:       Delivery Drivers for WooCommerce
- * Description:       This is a short description of what the plugin does. It's displayed in the WordPress admin area.
  * Plugin URI:        http://www.deviodigital.com
+ * Description:       Driver management for delivery services
  * Version:           1.0.0
- * Author:            WP Dispensary
- * Author URI:        https://www.wpdispensary.com
+ * Author:            Devio Digital
+ * Author URI:        http://www.deviodigital.com
  * License:           GPL-2.0+
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
  * Text Domain:       ddwc
