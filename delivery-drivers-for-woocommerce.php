f953a776b67456fb92549ed96af4e8b20973e467
Robert DeVore <deviodigital@gmail.com>
2018-09-17 10:27:32 -0400

    :rocket: RELEASE v1.0.1

--- delivery-drivers-for-woocommerce.php
+++ delivery-drivers-for-woocommerce.php
@@ -11,7 +11,7 @@
  * Plugin Name:       Delivery Drivers for WooCommerce
  * Plugin URI:        https://www.deviodigital.com
  * Description:       Driver management for delivery services
- * Version:           1.0.0
+ * Version:           1.0.1
  * Author:            Devio Digital
  * Author URI:        https://www.deviodigital.com
  * License:           GPL-2.0+
@@ -30,7 +30,7 @@ if ( ! defined( 'WPINC' ) ) {
  * Start at version 1.0.0 and use SemVer - https://semver.org
  * Rename this for your plugin and update it as you release new versions.
  */
-define( 'DDWC_VERSION', '1.0.0' );
+define( 'DDWC_VERSION', '1.0.1' );
 
 /**
  * The code that runs during plugin activation.
