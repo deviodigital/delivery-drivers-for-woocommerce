ad69ccb28c81c4313ceafc3404568d6e182f7792
Robert DeVore <deviodigital@gmail.com>
2019-05-15 20:26:08 -0400

    :ok_hand: IMPROVE: Removed unnecessary admin.js file

--- admin/class-ddwc-admin.php
+++ admin/class-ddwc-admin.php
@@ -76,21 +76,7 @@ class Delivery_Drivers_Admin {
 	 * @since    1.0.0
 	 */
 	public function enqueue_scripts() {
-
-		/**
-		 * This function is provided for demonstration purposes only.
-		 *
-		 * An instance of this class should be passed to the run() function
-		 * defined in Delivery_Drivers_Loader as all of the hooks are defined
-		 * in that particular class.
-		 *
-		 * The Delivery_Drivers_Loader will then create the relationship
-		 * between the defined hooks and the functions defined in this
-		 * class.
-		 */
-
-		wp_enqueue_script( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'js/ddwc-admin.js', array( 'jquery' ), $this->version, false );
-
+		//wp_enqueue_script( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'js/ddwc-admin.js', array( 'jquery' ), $this->version, false );
 	}
 
 }
