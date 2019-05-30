b0ee9677205706f42cea14c93dbc276ebb5c409a
Robert DeVore <deviodigital@gmail.com>
2019-05-29 21:17:06 -0400

    :ok_hand: IMPROVE: General code cleanup

--- admin/ddwc-woocommerce-settings.php
+++ admin/ddwc-woocommerce-settings.php
@@ -14,27 +14,27 @@ class Delivery_Drivers_WooCommerce_Settings {
 	 * Bootstraps the class and hooks required actions & filters.
 	 */
 	public static function init() {
-	   add_filter( 'woocommerce_settings_tabs_array', __CLASS__ . '::add_settings_tab', 50 );
-	   add_action( 'woocommerce_settings_tabs_ddwc', __CLASS__ . '::settings_tab' );
-	   add_action( 'woocommerce_update_options_ddwc', __CLASS__ . '::update_settings' );
-	   // Add custom type.
-	   add_action( 'woocommerce_admin_field_custom_type', __CLASS__ . '::output_custom_type', 10, 1 );
+		add_filter( 'woocommerce_settings_tabs_array', __CLASS__ . '::add_settings_tab', 50 );
+		add_action( 'woocommerce_settings_tabs_ddwc', __CLASS__ . '::settings_tab' );
+		add_action( 'woocommerce_update_options_ddwc', __CLASS__ . '::update_settings' );
+		// Add custom type.
+		add_action( 'woocommerce_admin_field_custom_type', __CLASS__ . '::output_custom_type', 10, 1 );
 	}
 
 	public static function output_custom_type( $value ) {
-	 	// You can output the custom type in any format you'd like.
+		// You can output the custom type in any format you'd like.
 		echo $value['desc'];
 	}
 
 	/**
 	 * Add a new settings tab to the WooCommerce settings tabs array.
 	 *
-	 * @param array $settings_tabs Array of WooCommerce setting tabs & their labels, excluding the Subscription tab.
-	 * @return array $settings_tabs Array of WooCommerce setting tabs & their labels, including the Subscription tab.
+	 * @param array $settings_tabs Array of WooCommerce setting tabs & their labels, excluding the Delivery Drivers tab.
+	 * @return array $settings_tabs Array of WooCommerce setting tabs & their labels, including the Delivery Drivers tab.
 	 */
 	public static function add_settings_tab( $settings_tabs ) {
-	   $settings_tabs['ddwc'] = __( 'Delivery Drivers', 'ddwc' );
-	   return $settings_tabs;
+		$settings_tabs['ddwc'] = __( 'Delivery Drivers', 'ddwc' );
+		return $settings_tabs;
 	}
 
 	/**
@@ -44,7 +44,7 @@ class Delivery_Drivers_WooCommerce_Settings {
 	 * @uses self::get_settings()
 	 */
 	public static function settings_tab() {
-	   woocommerce_admin_fields( self::get_settings() );
+		woocommerce_admin_fields( self::get_settings() );
 	}
 
 	/**
@@ -54,7 +54,7 @@ class Delivery_Drivers_WooCommerce_Settings {
 	 * @uses self::get_settings()
 	 */
 	public static function update_settings() {
-	   woocommerce_update_options( self::get_settings() );
+		woocommerce_update_options( self::get_settings() );
 	}
 
 	/**
@@ -84,51 +84,51 @@ class Delivery_Drivers_WooCommerce_Settings {
 		$settings = array(
 			// Section title.
 			'ddwc_settings_section_title' => array(
-			    'name' => __( 'Delivery Drivers for WooCommerce', 'ddwc' ),
-			    'type' => 'title',
-			    'desc' => __( 'Brought to you by <a href="https://www.deviodigital.com" target="_blank">Devio Digital</a>', 'ddwc' ),
-			    'id'   => 'ddwc_settings_section_title'
+				'name' => __( 'Delivery Drivers for WooCommerce', 'ddwc' ),
+				'type' => 'title',
+				'desc' => __( 'Brought to you by <a href="https://www.deviodigital.com" target="_blank">Devio Digital</a>', 'ddwc' ),
+				'id'   => 'ddwc_settings_section_title'
 			),
-            // Dispatch phone number.
-            'dispatch_phone_number' => array(
-                'name' => __( 'Dispatch phone number', 'ddwc' ),
-                'type' => 'text',
-                'desc' => __( 'Allow your drivers to call if they have questions about an order.', 'ddwc' ),
-                'id'   => 'ddwc_settings_dispatch_phone_number'
-            ),
-            // Google Maps API key.
-            'google_maps_api_key' => array(
-                'name' => __( 'Google Maps API key', 'ddwc' ),
-                'type' => 'text',
-                'desc' => __( 'Add a map to the order directions for your drivers.', 'ddwc' ),
-                'id'   => 'ddwc_settings_google_maps_api_key'
-            ),
-            // Driver ratings.
-            'driver_ratings' => array(
-                'name' => __( 'Driver ratings', 'ddwc' ),
-                'type' => 'select',
-                'desc' => __( 'Add driver details with delivery star ratings to order details page.', 'ddwc' ),
+			// Dispatch phone number.
+			'dispatch_phone_number' => array(
+				'name' => __( 'Dispatch phone number', 'ddwc' ),
+				'type' => 'text',
+				'desc' => __( 'Allow your drivers to call if they have questions about an order.', 'ddwc' ),
+				'id'   => 'ddwc_settings_dispatch_phone_number'
+			),
+			// Google Maps API key.
+			'google_maps_api_key' => array(
+				'name' => __( 'Google Maps API key', 'ddwc' ),
+				'type' => 'text',
+				'desc' => __( 'Add a map to the order directions for your drivers.', 'ddwc' ),
+				'id'   => 'ddwc_settings_google_maps_api_key'
+			),
+			// Driver ratings.
+			'driver_ratings' => array(
+				'name' => __( 'Driver ratings', 'ddwc' ),
+				'type' => 'select',
+				'desc' => __( 'Add driver details with delivery star ratings to order details page.', 'ddwc' ),
 				'id'   => 'ddwc_settings_driver_ratings',
 				'options' => array(
 					'yes' => 'Yes',
 					'no'  => 'No',
 				),
-            ),
-            // Driver phone number.
-            'driver_phone_number' => array(
-                'name' => __( 'Driver phone number', 'ddwc' ),
-                'type' => 'select',
-                'desc' => __( 'Add a button for customers to call driver in the driver details.', 'ddwc' ),
+			),
+			// Driver phone number.
+			'driver_phone_number' => array(
+				'name' => __( 'Driver phone number', 'ddwc' ),
+				'type' => 'select',
+				'desc' => __( 'Add a button for customers to call driver in the driver details.', 'ddwc' ),
 				'id'   => 'ddwc_settings_driver_phone_number',
 				'options' => array(
 					'yes' => 'Yes',
 					'no'  => 'No',
 				),
-            ),
+			),
 			// Section End.
 			'section_end' => array(
-			    'type' => 'sectionend',
-			    'id'   => 'ddwc_settings_section_end'
+				'type' => 'sectionend',
+				'id'   => 'ddwc_settings_section_end'
 			),
 		);
 		return apply_filters( 'ddwc_woocommerce_settings', $settings );
