0caf5df27858f083a7855cf988b2c495e9fb9c8b
Robert DeVore <deviodigital@gmail.com>
2019-06-16 14:57:03 -0400

    :package: NEW: Delivery Driver column on Edit Orders screen

--- admin/class-ddwc-admin.php
+++ admin/class-ddwc-admin.php
@@ -76,7 +76,146 @@ class Delivery_Drivers_Admin {
 	 * @since    1.0.0
 	 */
 	public function enqueue_scripts() {
-		//wp_enqueue_script( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'js/ddwc-admin.js', array( 'jquery' ), $this->version, false );
+		wp_enqueue_script( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'js/ddwc-admin.js', array( 'jquery' ), $this->version, false );
+		wp_localize_script( $this->plugin_name, 'WPaAjax', array( 'ajaxurl' => admin_url( 'admin-ajax.php' ) ) );
 	}
 
 }
+
+/**
+ * Add Deliver Driver Column to Orders screen
+ *
+ * @param [type] $columns
+ * @since 2.1
+ */
+function ddwc_show_custom_delivery_driver_column( $columns ) {
+
+    $new_columns = ( is_array( $columns ) ) ? $columns : array();
+
+	// Save shipping address column.
+	$shipping_address = $columns[ 'shipping_address' ];
+
+	// Remove shiping address column.
+	unset( $new_columns['shipping_address'] );
+
+	// Save order actions column.
+	$wc_actions = $columns[ 'wc_actions' ];
+
+	// Remove order actions column.
+	unset( $new_columns['wc_actions'] );
+
+    // Add delivery driver column.
+    $new_columns['delivery_driver'] = __( 'Delivery Driver', 'ddwc' );
+
+	// Add shipping address column.
+	$new_columns[ 'shipping_address' ] = $shipping_address;
+
+	// Add shipping address column.
+//	$new_columns[ 'wc_actions' ] = $wc_actions;
+
+	return $new_columns;
+}
+add_filter( 'manage_edit-shop_order_columns', 'ddwc_show_custom_delivery_driver_column', 21 );
+
+/**
+ * Delivery Driver column for Orders screen
+ *
+ * @param [type] $column
+ * @since 2.1
+ */
+function ddwc_custom_delivery_driver_column( $column ) {
+
+    global $post, $woocommerce, $the_order;
+
+    switch ( $column ) {
+
+        case 'delivery_driver' :
+			// Noncename needed to verify where the data originated.
+			echo '<input type="hidden" name="ddwc_meta_noncename" id="ddwc_meta_noncename" value="' .
+			wp_create_nonce( plugin_basename( __FILE__ ) ) . '" />';
+
+			// Get the driver data if its already been entered.
+			$ddwc_driver_id = get_post_meta( $post->ID, 'ddwc_driver_id', true );
+
+			// Echo Delivery Driver Metabox Input Field.
+			echo '<div class="ddwc-driver-box">';
+			wp_dropdown_users( array(
+				'show_option_none' => '--',
+				'role'             => 'driver',
+				'name'             => $post->ID,
+				'id'               => 'ddwc_driver_id',
+				'selected'         => $ddwc_driver_id,
+				'class'            => 'widefat',
+				'show'             => 'display_name_with_login'
+			) );
+			echo '</div>';
+
+        break;
+    }
+}
+add_action( 'manage_shop_order_posts_custom_column', 'ddwc_custom_delivery_driver_column', 21 );
+
+/**
+ * Add "no-link" class to tr's from WooCommerce orders screen
+ * 
+ * @since 2.1
+ */
+function ddwc_add_no_link_to_woocommerce_orders( $classes ) {
+	if ( current_user_can( 'manage_woocommerce' ) ) { //make sure we are shop managers 
+        foreach ( $classes as $class ) {
+	        if ( $class == 'type-shop_order' ) {
+	            $classes[] = 'no-link';
+	        }
+    	}
+    }
+    return $classes;
+}
+add_filter( 'post_class', 'ddwc_add_no_link_to_woocommerce_orders' );
+
+/**
+ * AJAX function to update driver ID on WooCommerce Orders page
+ * 
+ * @since 2.1
+ */
+function ddwc_delivery_driver_settings() {
+
+	$item_id     = $_POST['item_id'];
+	$metakey     = $_POST['metakey'];
+	$metavalue   = $_POST['metavalue'];
+
+	// Update driver ID for order.
+	update_post_meta( $item_id, $metakey, $metavalue );
+
+	// Get order.
+	$order = new WC_Order( $item_id );
+
+	// Update order status.
+	if ( -1 == $metavalue ) {
+		$order->update_status( 'processing' );
+	} else {
+		$order->update_status( 'driver-assigned' );
+	}
+
+	// WooCommerce product loop $args
+	$args = array(
+        'post_type'      => 'product',
+        'posts_per_page' => 1000,
+    );
+
+	// Get all products based on $args.
+    $loop = get_posts( $args );
+
+	// Loop through each product.
+	foreach ( $loop as $item ) {
+
+		// Update inventory.
+		if ( $metavalue === get_post_meta( $item->ID, $metakey, true ) ) {
+			update_post_meta( $item->ID, $metakey, $metavalue );
+		}
+
+	}
+
+    exit;
+}
+add_action( 'wp_ajax_ddwc_delivery_driver_settings', 'ddwc_delivery_driver_settings' );
+//add_action('wp_ajax_nopriv_ddwc_delivery_driver_settings', 'ddwc_delivery_driver_settings');
--- admin/js/ddwc-admin.js
+++ admin/js/ddwc-admin.js
@@ -1,32 +1,21 @@
-(function( $ ) {
-	'use strict';
+jQuery(document).ready(function ($) {
+	jQuery('select#ddwc_driver_id').change(function(e) {
+		var item_id =  $(this).attr('name');
+		var metakey =  $(this).attr('id');
+		var metavalue = this.value;
+		$.post(WPaAjax.ajaxurl,{
+			action : 'ddwc_delivery_driver_settings',
+			item_id : item_id,
+			metakey : metakey,
+			metavalue : metavalue
+//		},
+//		function(data, status) {
+//			console.log("Data: " + data + "\nStatus: " + status);
+		});
+	});
+});
 
-	/**
-	 * All of the code for your admin-facing JavaScript source
-	 * should reside in this file.
-	 *
-	 * Note: It has been assumed you will write jQuery code here, so the
-	 * $ function reference has been prepared for usage within the scope
-	 * of this function.
-	 *
-	 * This enables you to define handlers, for when the DOM is ready:
-	 *
-	 * $(function() {
-	 *
-	 * });
-	 *
-	 * When the window is loaded:
-	 *
-	 * $( window ).load(function() {
-	 *
-	 * });
-	 *
-	 * ...and/or other possibilities.
-	 *
-	 * Ideally, it is not considered best practise to attach more than a
-	 * single DOM-ready or window-load handler for a particular page.
-	 * Although scripts in the WordPress core, Plugins and Themes may be
-	 * practising this, we should strive to set a better example in our own work.
-	 */
-
-})( jQuery );
+// Remove the link click wrapper on WooCommerce Edit Orders screen.
+jQuery(document).ready(function ($) {
+	$('td.delivery_driver.column-delivery_driver a').click(function(){ return false });
+});
