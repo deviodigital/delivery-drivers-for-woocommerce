39e8afcf430a5b3f8ff7638d861b3aa4dcd87cc8
Robert DeVore <deviodigital@gmail.com>
2019-09-17 22:49:21 -0400

    :ok_hand: IMPROVE: General code cleanup

--- admin/ddwc-dashboard-shortcode.php
+++ admin/ddwc-dashboard-shortcode.php
@@ -66,7 +66,8 @@ function ddwc_dashboard_shortcode() {
 					// Get an instance of the WC_Order object
 					$order = wc_get_order( $_GET['orderid'] );
 
-					$order_data = $order->get_data(); // The Order data
+					// Get the order data.
+					$order_data = $order->get_data();
 
 					$order_id                   = $order_data['id'];
 					$order_parent_id            = $order_data['parent_id'];
@@ -85,26 +86,10 @@ function ddwc_dashboard_shortcode() {
 					$order_total                = $order_data['total'];
 					$order_total_tax            = $order_data['total_tax'];
 					$order_customer_id          = $order_data['customer_id'];
-					$order_billing_first_name   = $order_data['billing']['first_name'];
-					$order_billing_last_name    = $order_data['billing']['last_name'];
-					$order_billing_company      = $order_data['billing']['company'];
-					$order_billing_address_1    = $order_data['billing']['address_1'];
-					$order_billing_address_2    = $order_data['billing']['address_2'];
-					$order_billing_city         = $order_data['billing']['city'];
-					$order_billing_state        = $order_data['billing']['state'];
-					$order_billing_postcode     = $order_data['billing']['postcode'];
-					$order_billing_country      = $order_data['billing']['country'];
-					$order_billing_email        = $order_data['billing']['email'];
-					$order_billing_phone        = $order_data['billing']['phone'];
 					$order_shipping_first_name  = $order_data['shipping']['first_name'];
 					$order_shipping_last_name   = $order_data['shipping']['last_name'];
-					$order_shipping_company     = $order_data['shipping']['company'];
-					$order_shipping_address_1   = $order_data['shipping']['address_1'];
-					$order_shipping_address_2   = $order_data['shipping']['address_2'];
-					$order_shipping_city        = $order_data['shipping']['city'];
-					$order_shipping_state       = $order_data['shipping']['state'];
-					$order_shipping_postcode    = $order_data['shipping']['postcode'];
-					$order_shipping_country     = $order_data['shipping']['country'];
+					$order_billing_first_name   = $order_data['billing']['first_name'];
+					$order_billing_last_name    = $order_data['billing']['last_name'];
 
 					echo '<div class="ddwc-orders">';
 
