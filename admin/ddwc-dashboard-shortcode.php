db719c67e336cb60721733f0e31f6cd3db0463a5
Robert DeVore <deviodigital@gmail.com>
2019-12-31 16:47:02 -0500

    :ok_hand: IMPROVE: Added the ddwc_delivery_address_google_map_geocode function before the Google Map

--- admin/ddwc-dashboard-shortcode.php
+++ admin/ddwc-dashboard-shortcode.php
@@ -152,6 +152,18 @@ function ddwc_dashboard_shortcode() {
 							add_filter( 'woocommerce_order_formatted_billing_address' , 'ddwc_custom_order_formatted_address' );
 							$delivery_address = str_replace( '<br/>', ' ', $order->get_formatted_billing_address() );
 						}
+
+						/**
+						 * Google Maps Delivery Address Geocode
+						 * 
+						 * This function will check admin settings and change the $delivery_address variable
+						 * to lat/lng coordinates if activated by the administrator
+						 * 
+						 * @return string
+						 * @since 2.7
+						 */
+						ddwc_delivery_address_google_map_geocode( $delivery_address );
+
 						// Create the Google Map.
 						$google_map = '<iframe width="600" height="450" frameborder="0" style="border:0" src="https://www.google.com/maps/embed/v1/directions?origin=' . apply_filters( 'ddwc_google_maps_origin_address', $store_address ) . '&destination=' . apply_filters( 'ddwc_google_maps_delivery_address', $delivery_address ) . '&key=' . get_option( 'ddwc_settings_google_maps_api_key' ) . '" allowfullscreen></iframe>';
 
