35a793a74f2f8b88b79da65c0b0bc6438f6828e8
Robert DeVore <deviodigital@gmail.com>
2020-08-16 09:02:27 -0400

    :ok_hand: IMPROVE: Added driver picture to admin driver dashboard table

--- admin/ddwc-functions.php
+++ admin/ddwc-functions.php
@@ -400,8 +400,13 @@ function ddwc_driver_dashboard_admin_drivers_table() {
 				$email_address = '<a href="mailto:' . esc_html( $driver_email ) . '" class="button ddwc-button"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24"><path d="M0 3v18h24v-18h-24zm6.623 7.929l-4.623 5.712v-9.458l4.623 3.746zm-4.141-5.929h19.035l-9.517 7.713-9.518-7.713zm5.694 7.188l3.824 3.099 3.83-3.104 5.612 6.817h-18.779l5.513-6.812zm9.208-1.264l4.616-3.741v9.348l-4.616-5.607z"/></svg></a>';
 			}
 
+			if ( get_user_meta( $driver->ID, 'ddwc_driver_picture', true ) ) {
+				$driver_pic = get_user_meta( $driver->ID, 'ddwc_driver_picture', true );
+				$driver_img = '<a href="' . $driver_pic['url'] . '"><img src="' . $driver_pic['url'] . '" alt="' . esc_html( $driver->display_name ) . '" /></a>';
+			}
+
 			$tbody = array(
-				esc_html( $driver->display_name ) . ' <a href="' . admin_url( 'user-edit.php?user_id=' . $driver->ID ) . '">(' . __( 'edit', 'ddwc' ) . ')</a>',
+				$driver_img . '<span class="driver-name">' . esc_html( $driver->display_name ) . '</span> <a href="' . admin_url( 'user-edit.php?user_id=' . $driver->ID ) . '">(' . __( 'edit', 'ddwc' ) . ')</a>',
 				$availability,
 				$driver_rating_final,
 				$email_address . $phone_number,
