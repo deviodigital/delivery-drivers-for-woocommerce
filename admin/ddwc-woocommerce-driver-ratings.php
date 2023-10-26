27063038b72472b78e7b03abdc8fdad3e205a7ba
Robert DeVore <deviodigital@gmail.com>
2023-10-26 14:14:16 -0400

    :ok_hand: IMPROVE: Updated driver display on order details to show/hide vehicle details

--- admin/ddwc-woocommerce-driver-ratings.php
+++ admin/ddwc-woocommerce-driver-ratings.php
@@ -19,6 +19,8 @@ if ( ! defined( 'WPINC' ) ) {
 /**
  * Add Driver details to customer's order
  * 
+ * @TODO Update this to be in a separate file
+ * 
  * @param object $order 
  *
  * @since  1.6
@@ -57,8 +59,11 @@ function ddwc_order_driver_details( $order ) {
         $string .= '<h4>' . $driver_meta->user_firstname . ' ' . $driver_meta->user_lastname . '</h4>';
         // Display star rating.
         if ( 'completed' == $order_status ) {
-            // Star ratings.
-            $string .= esc_attr__( 'Rate Delivery', 'delivery-drivers-for-woocommerce' ) . '<br /><select class="driver-rating" id="rating_' . $order_id . '" data-id="rating_' . $order_id . '"><option value=""></option><option value="1">1</option><option value="2">2</option><option value="3">3</option><option value="4">4</option><option value="5">5</option></select><span class="rating-tooltip">' . apply_filters( 'ddwc_driver_rating_thank_you_text', esc_attr__( 'Thanks for your rating!', 'delivery-drivers-for-woocommerce' ) ) . '</span>';
+            // Display Driver Ratings if WooCommerce setting isn't set to NO.
+            if ( 'no' !== get_option( 'ddwc_settings_driver_ratings' ) ) {
+                // Star ratings.
+                $string .= esc_attr__( 'Rate Delivery', 'delivery-drivers-for-woocommerce' ) . '<br /><select class="driver-rating" id="rating_' . $order_id . '" data-id="rating_' . $order_id . '"><option value=""></option><option value="1">1</option><option value="2">2</option><option value="3">3</option><option value="4">4</option><option value="5">5</option></select><span class="rating-tooltip">' . apply_filters( 'ddwc_driver_rating_thank_you_text', esc_attr__( 'Thanks for your rating!', 'delivery-drivers-for-woocommerce' ) ) . '</span>';
+            }
         } elseif ( 'driver-assigned' == $order_status || 'out-for-delivery' == $order_status ) {
             // Display driver's phone number.
             if ( 'no' !== get_option( 'ddwc_settings_driver_phone_number' ) ) {
@@ -72,34 +77,40 @@ function ddwc_order_driver_details( $order ) {
             // Do nothing.
         }
         $string .= '</div>';
-        // Display driver details table if order is not completed.
-        if ( 'driver-assigned' == $order_status || 'out-for-delivery' == $order_status ) {
-            // Driver details table.
-            $string .= '<table class="ddwc-driver-details"><tbody><tr>';
-            // Vehicle color.
-            if ( get_user_meta( $driver_id, 'ddwc_driver_vehicle_color', true ) ) {
-                // Color name.
-                $color_name = esc_attr__( 'Vehicle Color', 'delivery-drivers-for-woocommerce' );
-                if ( '' != get_user_meta( $driver_id, 'ddwc_driver_transportation_type', true ) ) {
-                    $color_name = get_user_meta( $driver_id, 'ddwc_driver_transportation_type', true ) . ' Color';
+
+        // Make sure the driver vehicle details should be displayed.
+        if ( 'no' !== get_option( 'ddwc_settings_driver_vehicle_details' ) ) {
+            // Display driver details table if order is not completed.
+            if ( 'driver-assigned' == $order_status || 'out-for-delivery' == $order_status ) {
+                // Driver details table.
+                $string .= '<table class="ddwc-driver-details"><tbody><tr>';
+                // Vehicle color.
+                if ( get_user_meta( $driver_id, 'ddwc_driver_vehicle_color', true ) ) {
+                    // Color name.
+                    $color_name = esc_attr__( 'Vehicle Color', 'delivery-drivers-for-woocommerce' );
+                    if ( '' != get_user_meta( $driver_id, 'ddwc_driver_transportation_type', true ) ) {
+                        $color_name = get_user_meta( $driver_id, 'ddwc_driver_transportation_type', true ) . ' Color';
+                    }
+                    $string .= '<td>' . $color_name . '<br /><strong>' . get_user_meta( $driver_id, 'ddwc_driver_vehicle_color', true ) . '</strong></td>';
                 }
-                $string .= '<td>' . $color_name . '<br /><strong>' . get_user_meta( $driver_id, 'ddwc_driver_vehicle_color', true ) . '</strong></td>';
-            }
-            // Vehicle model.
-            if ( get_user_meta( $driver_id, 'ddwc_driver_vehicle_model', true ) ) {
-                // Model name.
-                $model_name = esc_attr__( 'Vehicle Model', 'delivery-drivers-for-woocommerce' );
-                if ( '' != get_user_meta( $driver_id, 'ddwc_driver_transportation_type', true ) ) {
-                    $model_name = get_user_meta( $driver_id, 'ddwc_driver_transportation_type', true ) . ' Model';
+                // Vehicle model.
+                if ( get_user_meta( $driver_id, 'ddwc_driver_vehicle_model', true ) ) {
+                    // Model name.
+                    $model_name = esc_attr__( 'Vehicle Model', 'delivery-drivers-for-woocommerce' );
+                    if ( '' != get_user_meta( $driver_id, 'ddwc_driver_transportation_type', true ) ) {
+                        $model_name = get_user_meta( $driver_id, 'ddwc_driver_transportation_type', true ) . ' Model';
+                    }
+                    $string .= '<td>' . $model_name . '<br /><strong>' . get_user_meta( $driver_id, 'ddwc_driver_vehicle_model', true ) . '</strong></td>';
                 }
-                $string .= '<td>' . $model_name . '<br /><strong>' . get_user_meta( $driver_id, 'ddwc_driver_vehicle_model', true ) . '</strong></td>';
+                // Driver License plate.
+                if ( get_user_meta( $driver_id, 'ddwc_driver_license_plate', true ) ) {
+                    $string .= '<td>' . esc_attr__( 'License Plate', 'delivery-drivers-for-woocommerce' ) . '<br /><strong>' . get_user_meta( $driver_id, 'ddwc_driver_license_plate', true ) . '</strong></td>';
+                }
+                $string .= '</tr></tbody></table>';
             }
-            // Driver License plate.
-            if ( get_user_meta( $driver_id, 'ddwc_driver_license_plate', true ) ) {
-                $string .= '<td>' . esc_attr__( 'License Plate', 'delivery-drivers-for-woocommerce' ) . '<br /><strong>' . get_user_meta( $driver_id, 'ddwc_driver_license_plate', true ) . '</strong></td>';
-            }
-            $string .= '</tr></tbody></table>';
+
         }
+
         $string .= '</div>';
 
         echo $string;
@@ -108,50 +119,50 @@ function ddwc_order_driver_details( $order ) {
          * @todo Move JavaScript to public JS file and pass $ddwc_delivery_rating to it
          */
 
-        // Get the ajax rating file.
-        $ddwc_delivery_rating = get_post_meta( $order_id, 'ddwc_delivery_rating', true );
-        ?>
-    <script type="text/javascript">
-    $(function() {
-        $('.driver-rating').barrating({
-            theme: 'fontawesome-stars',
-            initialRating: "<?php esc_html_e( $ddwc_delivery_rating ); ?>",
-            showSelectedRating: true,
-            // onSelect is what triggers the saving of the rating.
-            onSelect: function(value, text, event) {
-                // Get element id by data-id attribute
-                var el = this;
-                var el_id = el.$elem.data('id');
-                // rating was selected by a user
-                if (typeof(event) !== 'undefined') {
-                    var split_id = el_id.split("_");
-                    var postid = split_id[1]; // postid.
-                    // Sending data to the ddwc_driver_rating function.
-                    $.post(WPaAjax.ajaxurl, {
-                        action: 'ddwc_driver_rating',
-                        postid: postid,
-                        rating: value
-                    }, function(response) {
-                        console.log(response);
-                    });
-                }
-                $("span.rating-tooltip").addClass("rated");
-                setTimeout(function() {
-                    $("span.rating-tooltip").removeClass("rated");
-                }, 1600 );
-            } // end onSelect
-        });
-    });
-    </script>
+        // Display Driver Ratings if WooCommerce setting isn't set to NO.
+        if ( 'no' !== get_option( 'ddwc_settings_driver_ratings' ) ) {
+
+            // Get the ajax rating file.
+            $ddwc_delivery_rating = get_post_meta( $order_id, 'ddwc_delivery_rating', true );
+            ?>
+            <script type="text/javascript">
+            $(function() {
+                $('.driver-rating').barrating({
+                    theme: 'fontawesome-stars',
+                    initialRating: "<?php esc_html_e( $ddwc_delivery_rating ); ?>",
+                    showSelectedRating: true,
+                    // onSelect is what triggers the saving of the rating.
+                    onSelect: function(value, text, event) {
+                        // Get element id by data-id attribute
+                        var el = this;
+                        var el_id = el.$elem.data('id');
+                        // rating was selected by a user
+                        if (typeof(event) !== 'undefined') {
+                            var split_id = el_id.split("_");
+                            var postid = split_id[1]; // postid.
+                            // Sending data to the ddwc_driver_rating function.
+                            $.post(WPaAjax.ajaxurl, {
+                                action: 'ddwc_driver_rating',
+                                postid: postid,
+                                rating: value
+                            }, function(response) {
+                                //console.log(response);
+                            });
+                        }
+                        $("span.rating-tooltip").addClass("rated");
+                        setTimeout(function() {
+                            $("span.rating-tooltip").removeClass("rated");
+                        }, 1600 );
+                    } // end onSelect
+                });
+            });
+            </script>
+            <?php } ?>
     <?php } else {
         // Do nothing.
     }
 }
-
-// Display Driver Ratings if WooCommerce setting isn't set to NO.
-if ( 'no' !== get_option( 'ddwc_settings_driver_ratings' ) ) {
-    add_action( 'woocommerce_order_details_after_order_table', 'ddwc_order_driver_details' );
-}
+add_action( 'woocommerce_order_details_after_order_table', 'ddwc_order_driver_details' );
 
 /**
  * AJAX function to update the delivery driver's rating on an order.
